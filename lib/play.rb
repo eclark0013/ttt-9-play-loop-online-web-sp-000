7a0a0f1143287853334a1c4112da350dca9daa2e
Eric Clark <eclark0013@gmail.com>
2019-07-06 15:31:57 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  until board.all? {|spot| position_taken?(spot)}
+  until board.all? {|spot| position_taken?(board, spot)}
     turn(board)
   end
 end
